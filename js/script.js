edea1af3cee2ff864c3bc08becbbf45846d32b6b
renzien <alifrizki1011@gmail.com>
2022-05-17 07:55:40 +0700

    update: border-radius

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(window).scroll(function() {
     if ($(window).scrollTop() >= 50) {
         $('.navbar').css('background', '#ffa825');
+        $('.navbar').css('border-bottom-left-radius', '32px');
+        $('.navbar').css('border-bottom-right-radius', '32px');
         $('.navbar').css('box-shadow', '0px 2px 4px rgba(0, 0, 0, 0.5)');
     } else {
         $('.navbar').css('background', 'transparent');
