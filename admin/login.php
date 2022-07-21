ed37847b64adfc4e4d26a3e7c0a6712094e43ab4
renzien <alifrizki1011@gmail.com>
2022-07-21 15:57:50 +0700

    add: login

---
+++ admin/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <div class="center">
+        <h1>Login</h1>
+        <form action="POST">
+            <div class="txt_field">
+                <input type="text" required>
+                <span></span>
+                <label>Username</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" required>
+                <span></span>
+                <label>Password</label>
+            </div>
+            <input type="submit" value="Login">
+            <div class="copyright">
+                &copy; Mbah Kakung Ayam Geprek 2022
+            </div>
+        </form>
+    </div>
+</body>
+</html>
